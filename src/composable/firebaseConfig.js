5b42f0f1144a783628420b0881bb386556f35761
Edgar Alonzo <ragde956@gmail.com>
2021-11-19 23:21:54 -0600

    fixed firebase error locally

---
+++ src/composable/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBv648QG70vh7EzjWeNkE47CeyX0pqmkvk",
+    authDomain: "vue-3-login-test-47384.firebaseapp.com",
+    projectId: "vue-3-login-test-47384",
+    storageBucket: "vue-3-login-test-47384.appspot.com",
+    messagingSenderId: "802676056283",
+    appId: "1:802676056283:web:c5394b790297752d3f2cfe",
+    measurementId: "G-JFT77XWWP8"
+  
+};
